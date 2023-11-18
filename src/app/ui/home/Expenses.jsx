5faaf3262b2faf08dae1c7aa03ac314589e43c91
Tiven <vairtor@gmail.com>
2023-11-18 16:55:05 -0300

    Fix balance expenses incomes width

--- src/app/ui/home/Expenses.jsx
+++ src/app/ui/home/Expenses.jsx
@@ -2,7 +2,7 @@ import MoneySend from "../icons/MoneySend";
 
 export default function Expenses({ expenses }) {
 	return (
-		<article className="bg-white p-7 rounded-2xl w-72 relative">
+		<article className="bg-white p-7 rounded-2xl w-64 relative">
 			<h3 className="text-center text-black text-2xl p-1">EXPENSES</h3>
 			<p className="text-center text-black text-2xl p-1">${expenses}</p>
 			<div className="bg-[#00EFC5] w-12 h-12 flex justify-center items-center rounded-full absolute -left-5 bottom-10">
--- src/app/ui/home/Income.jsx
+++ src/app/ui/home/Income.jsx
@@ -2,7 +2,7 @@ import MoneyRecive from "../icons/MoneyRecive";
 
 export default function Income({ incomes }) {
 	return (
-		<article className="bg-white p-7 rounded-2xl w-72 relative">
+		<article className="bg-white p-7 rounded-2xl w-64 relative">
 			<h3 className="text-center text-black text-2xl p-1">INCOMES</h3>
 			<p className="text-center text-black text-2xl p-1">${incomes}</p>
 			<div className="bg-[#00EFC5] w-12 h-12 flex justify-center items-center rounded-full absolute -left-5 bottom-10">
