3d413813e92df25320ecaf0985dd6c9cef1f89e6
Tiven <vairtor@gmail.com>
2023-11-19 13:52:59 -0300

    add: shadow to balance, incomes, expenses

--- src/app/ui/home/Balance.jsx
+++ src/app/ui/home/Balance.jsx
@@ -2,7 +2,7 @@ import WalletMoney from "../icons/WalletMoney";
 
 export default function Balance({ balance }) {
 	return (
-		<article className="bg-white p-7 rounded-2xl w-64 relative">
+		<article className="bg-white p-6 rounded-2xl w-56 relative shadow-g100">
 			<h3 className="text-center text-black text-2xl p-1">BALANCE</h3>
 			<p className="text-center text-black text-2xl p-1">${balance}</p>
 			<div className="bg-[#00EFC5] w-12 h-12 flex justify-center items-center rounded-full absolute -left-5 bottom-10">
